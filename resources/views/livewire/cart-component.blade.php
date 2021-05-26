b4bc6aa45537abdc895cdd03540e072e650b0c83
Supriya <supriyamaanvi91170@gmail.com>
2021-05-27 01:35:40 +0530

    Inc&DecCarQtyDone

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -31,8 +31,8 @@
                         <div class="quantity">
                             <div class="quantity-input">
                                 <input type="text" name="product-quatity" value="{{$cart_product->qty}}" data-max="120" pattern="[0-9]*" >									
-                                <a class="btn btn-increase" href="#"></a>
-                                <a class="btn btn-reduce" href="#"></a>
+                                <a class="btn btn-increase" href="#" wire:click.prevent="increaseQuantity('{{$cart_product->rowId}}')"></a>
+                                <a class="btn btn-reduce" href="#" wire:click.prevent="decreaseQuantity('{{$cart_product->rowId}}')"></a>
                             </div>
                         </div>
                         <div class="price-field sub-total"><p class="price">${{$cart_product->subtotal}}</p></div>
