9d63533bf2f0caf03a919c36f69494d5c6fdcf75
Supriya <supriyamaanvi91170@gmail.com>
2021-05-27 22:36:32 +0530

    PerPageProduct&SortingDone

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -9,6 +9,13 @@ use Cart;
 
 class ShopComponent extends Component
 {
+    public $sorting;
+    public $pagesize;
+
+    public function mount(){
+        $this->sorting = "default";
+        $this->pagesize = 12;
+    }
     public function store($product_id,$product_name,$product_price){
         Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in cart');
@@ -17,7 +24,18 @@ class ShopComponent extends Component
     use WithPagination;
     public function render()
     {
-        $products = product::Paginate(12);
+        if($this->sorting=='date'){
+            $products = Product::orderBy('created_at','DESC')->Paginate($this->pagesize); 
+        }
+        else if($this->sorting=='price'){
+            $products = Product::orderBy('regular_price','ASC')->Paginate($this->pagesize); 
+        }
+        if($this->sorting=='price-desc'){
+            $products = Product::orderBy('regular_price','DESC')->Paginate($this->pagesize); 
+        }
+        else{
+            $products = Product::Paginate($this->pagesize);
+        }       
         return view('livewire.shop-component',['products'=>$products])->layout('layouts.base');
     }
 }
