5ec4153887548111e1ec6f2a908c0657a4f8b667
Supriya <supriyamaanvi91170@gmail.com>
2021-06-07 02:31:13 +0530

    show all wishlisted item

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Livewire\CheckoutComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
 use App\Http\Livewire\SearchComponent;
+use App\Http\Livewire\WishlistComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
@@ -44,6 +45,7 @@ Route::get('/checkout',CheckoutComponent::class);
 Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
 Route::get('/product-category/{category_slug}',CategoryComponent::class)->name('product.category');
 Route::get('/search',SearchComponent::class)->name('product.search');
+Route::get('/wishlist',WishlistComponent::class)->name('product.wishlist');
 
 //for customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
