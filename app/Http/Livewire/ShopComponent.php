001bd904a748e10b39e82205a48bebe7ddef5d40
Supriya <supriyamaanvi91170@gmail.com>
2021-06-07 01:49:04 +0530

    remove from wishlist done

--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -24,26 +24,42 @@ class ShopComponent extends Component
         $this->min_price = 1;
         $this->max_price = 1000;
     }
-    public function store($product_id,$product_name,$product_price){
+    public function store($product_id,$product_name,$product_price)
+    {
         Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in cart');
         return redirect()->route('product.cart');
     }
-    public function addToWishlist($product_id,$product_name,$product_price){
+    public function addToWishlist($product_id,$product_name,$product_price)
+    {
         Cart::instance('wishlist')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         $this->emitTo('wishlist-count-component','refreshComponent');
+    }
+    public function removeFromWishlist($product_id)
+    {
+        foreach(Cart::instance('wishlist')->content() as $witem)
+        {
+            if($witem->id == $product_id)
+            {
+                Cart::instance('wishlist')->remove($witem->rowId);
+                $this->emitTo('wishlist-count-component','refreshComponent');    
+            }
+        }
     }
 
     use WithPagination;
     public function render()
     {
-        if($this->sorting=='date'){
+        if($this->sorting=='date')
+        {
             $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('created_at','DESC')->Paginate($this->pagesize); 
         }
-        else if($this->sorting=='price'){
+        else if($this->sorting=='price')
+        {
             $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price','ASC')->Paginate($this->pagesize); 
         }
-        if($this->sorting=='price-desc'){
+        if($this->sorting=='price-desc')
+        {
             $products = Product::whereBetween('regular_price',[$this->min_price,$this->max_price])->orderBy('regular_price','DESC')->Paginate($this->pagesize); 
         }
         else{
